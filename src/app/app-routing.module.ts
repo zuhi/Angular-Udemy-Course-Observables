9f3387592ddaca788f78ae4b942a8338b0858632
Ankita Shreya <ankita9shreya@gmail.com>
2020-09-03 13:28:07 +0530

    Basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
